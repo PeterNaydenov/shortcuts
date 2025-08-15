8bfa5125779da82f963967ca6ffb197e9b7bbab7
Peter Naydenov <peter.naydenov@gmail.com>
2025-08-15 10:24:33 +0300

    Add some inputs to test 'form' plugin

--- test-components/Block.jsx
+++ test-components/Block.jsx
@@ -3,6 +3,8 @@ return <>
     <div className="block" data-click="red"><span id='rspan'>Red</span> </div>
     <button className="big-btn" data-click="mega">Mega button</button>
     <p>Some text with <a href="#" target="_blank">link <span id="anchor">in</span></a> it</p>
+    <p><input id="name" type="text" /></p>
+    <p><input type="text" id="age" /></p>
 </> 
 }
 
