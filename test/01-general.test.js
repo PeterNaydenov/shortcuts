a0c32aee1aeb5c91ef2ff2f8afd2316e207be85e
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-15 23:22:06 +0300

    Cleanup...

--- test/01-general.test.js
+++ test/01-general.test.js
@@ -5,6 +5,19 @@ import Block from '../test-components/Block.jsx'
 import VisaulController from '@peter.naydenov/visual-controller-for-react'
 import '../test-components/style.css'
 
+
+import {
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+  // Tip: all queries are also exposed on an object
+  // called "queries" which you could import here as well
+  waitFor,
+} from '@testing-library/dom'
+
+
+
 import { 
           pluginClick,
           pluginKey
@@ -107,7 +120,7 @@ test ( 'Simple shortcut', () => {
                                     short.enablePlugin ( pluginKey )
                                     short.changeContext ()
                                     short.changeContext ( 'general'  )
-                                    userEvent.keyboard ( '{Shift>}a</Shift>' )
+                                    userEvent.keyboard ( '{Shift>}a</Shift>' ) // Write a with shift
                                     expect ( a ).to.be.false
                                     wait ( 1000 ) 
                                        .then ( () => { // Default wait sequence timeout is 480 ms, but maxSequence is 1, so we don't need to wait for timeout
