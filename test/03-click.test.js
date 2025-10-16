b9b7e497c7b244b815539011a69a0d0240bda3f7
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-16 09:58:07 +0300

    Test: Files for testing shortcuts plugins

---
+++ test/03-click.test.js
@@ -0,0 +1,31 @@
+import { beforeEach, describe, it, test } from 'vitest'
+import { userEvent } from '@vitest/browser/context'
+
+import Block from '../test-components/Block.jsx'
+import VisaulController from '@peter.naydenov/visual-controller-for-react'
+import '../test-components/style.css'
+
+
+import {
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+  // Tip: all queries are also exposed on an object
+  // called "queries" which you could import here as well
+  waitFor,
+} from '@testing-library/dom'
+
+
+
+import { 
+          pluginClick,
+          pluginKey
+        , pluginForm
+        , shortcuts 
+                } from '../src/main.js'
+import { expect } from 'chai'
+
+import askForPromise from 'ask-for-promise'
+
+const html = new VisaulController ();
