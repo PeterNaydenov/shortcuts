1d298a6a4dc574330c1a3c46d715de1d1072d7d0
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-18 17:25:16 +0300

    Test: Vitest configuration updates. Workspace files are depricated.
    Workspace files are deprecated, so time to go. Configuration was moved to 'vitest.config.js'

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig ({
+      plugins: [react()],
       test: {
             coverage: {
                         reporter: ['lcov', 'text-summary']
-                  }
+                  },
+            browser: {
+              enabled: true,
+              headless: true,
+              provider: 'playwright',
+              instances: [
+                {
+                  browser: 'chromium'
+                }
+              ]
+            }
         }
 })
