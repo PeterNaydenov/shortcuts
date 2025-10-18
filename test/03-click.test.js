ed1659ec654fb046688532019a9948b9ea519025
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-18 17:36:56 +0300

    Test: Update click tests

--- test/03-click.test.js
+++ test/03-click.test.js
@@ -151,18 +151,19 @@ describe ( 'Click plugin', () => {
 
 
       it ( 'Triple left click', async () => {
-                          expect ( a ).to.equal ( false )
                           short.enablePlugin ( pluginClick )
-                          short.changeContext ( 'touch' )
+                          const hitItem = document.querySelector ( '#rspan' );
+                          expect ( a ).to.equal ( false )
                           // Load will restart the selected context
                           short.load ({ 
                                       // load will overwrite existing 'touch' context definition
-                                      'touch' : {   
+                                      'mama' : {   
                                                   'click: left-3' : () => a = true
                                               } 
-                                }) 
-                          await userEvent.tripleClick ( document.querySelector ( '#rspan' ) )
-                          await wait ( 20 )
+                                })
+                          short.changeContext ( 'mama' )
+                          await wait ( 12 )
+                          await userEvent.tripleClick ( hitItem )
                           // Default wait mouse timeout is 320 ms, but maxClicks is set to 3, 
                           // so we don't need to wait for timeout
                           await waitFor ( () => {
@@ -193,24 +194,26 @@ describe ( 'Click plugin', () => {
         
 
       
-      it ( 'Arguments of click handler', async () => {
-                /**
-                *    Need to know arguments for 'click' handler
-                *    function myMouseHandler ({
-                *                         context     // (string) Name of the current context;
-                *                       , note        // (string) Name of the note or null if note isn't set;
-                *                       , dependencies // (object) Object with dependencies that you have set by calling `setDependencies` method;
-                *                       , target      // (DOM element). Target element of the mouse event;
-                *                       , targetProps // (object). Coordinates of the target element (top, left, right, bottom, width, height) or null if target element is not available;
-                *                       , x           // (number). X coordinate of the target element;
-                *                       , y           // (number). Y coordinate of the target element;
-                *                       , event       // (object). Original mouse event object;
-                *                  }) {
-                *            // Body of the handler. Do something...
-                *       }
-                */
-                let test = [];
-                let i = 0;
+       it ( 'Arguments of click handler', async () => {
+                 /**
+                 *    Need to know arguments for 'click' handler
+                 *    function myMouseHandler ({
+                 *                         context     // (string) Name of the current context;
+                 *                       , note        // (string) Name of the note or null if note isn't set;
+                 *                       , dependencies // (object) Object with dependencies that you have set by calling `setDependencies` method;
+                 *                       , target      // (DOM element). Target element of the mouse event;
+                 *                       , targetProps // (object). Coordinates of the target element (top, left, right, bottom, width, height) or null if target element is not available;
+                 *                       , x           // (number). X coordinate of the target element;
+                 *                       , y           // (number). Y coordinate of the target element;
+                 *                       , event       // (object). Original mouse event object;
+                 *                  }) {
+                 *            // Body of the handler. Do something...
+                 *       }
+                 */
+                 // Ensure clean state for this test
+                 let megaBtn = document.querySelector ( '[data-click="mega"]' )
+                 let test = [];
+                 let i = 0;
                 short.enablePlugin ( pluginClick )
                 short.setDependencies ({ test })
                 short.load ({
@@ -234,17 +237,20 @@ describe ( 'Click plugin', () => {
                                                           ;
                                                       result.target = target.dataset.click
                                                       test.push ( result )
-                                                      console.log ( targetProps)
                                                       i++
                                                 }
                             } // local
                       })
-                short.changeContext ( 'local' )
-                await userEvent.click ( document.querySelector ( '[data-click="mega"]' ) )
-                expect ( i ).to.be.equal ( 1 )
-                let result = test[0];
-                expect ( result.target ).to.be.equal ( 'mega' )
-                expect ( result.context ).to.be.equal ( 'local' )
+                 short.changeContext ( 'local' )
+                 expect ( megaBtn ).to.not.be.null
+                 await userEvent.click ( megaBtn )
+                 await wait ( 50 )  // Wait for click processing
+                 await waitFor ( () => {
+                              expect ( i ).to.be.equal ( 1 )      
+                              let result = test[0];
+                              expect ( result.target ).to.be.equal ( 'mega' )
+                              expect ( result.context ).to.be.equal ( 'local' )
+                        }, { timeout: 1000, interval: 12 })
           }) // it arguments of click handler
 
 
@@ -267,5 +273,48 @@ describe ( 'Click plugin', () => {
                 if ( loc )   await userEvent.click ( loc )
                 expect ( result ).to.be.equal ( 'A' )  
           }) // it click on anchor
+
+
+
+      it ( 'Mute click plugin', async () => {
+                                const 
+                                      result = []
+                                    , trg = document.querySelector ( '#rspan' ) 
+                                    ;
+                                
+                                let i = 0;
+                                result.push ( 'init' )
+
+                                
+                                short.load ({
+                                      'local' : {
+                                              'click: left-1 ' : ({dependencies}) => {
+                                                        let { result } = dependencies;
+                                                        result.push ( i++ )
+                                                    }
+                                            }
+                                    })
+                                short.setDependencies ({ result })
+                                short.enablePlugin ( pluginClick )
+                                short.changeContext ( 'local' )
+
+
+                                await userEvent.click ( trg )
+                                await wait( 330 )
+                                await waitFor ( () => {
+                                          // We checking if the shortcut works
+                                          expect ( result ).to.have.lengthOf ( 2 )
+                                          expect ( i ).to.equal ( 1 )
+                                    }, { timeout: 1000, interval: 12 })
+
+                                short.mutePlugin ( 'click' )
+
+                                await userEvent.click ( trg )
+                                await waitFor ( () => {
+                                          // Plugin is muted, so we don't expect any changes
+                                          expect ( result ).to.have.lengthOf ( 2 )
+                                          expect ( i ).to.equal ( 1 )
+                                    }, { timeout: 1000, interval: 12 })
+              }) // it mute click plugin
   
 }) // describe
