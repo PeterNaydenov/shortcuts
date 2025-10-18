e78b55f52101e64cf1871611b084bf185ccc7800
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-18 21:19:16 +0300

    Fix: System error event is '@shortcuts-error';

--- src/methods/unload.js
+++ src/methods/unload.js
@@ -14,11 +14,11 @@ const
 return function unload ( contextName ) {
         const current = currentContext.name;
         if ( current === contextName ) {
-                    ev.emit ( 'shortcuts-error', `Context '${ contextName }' can't be removed during is current active context. Change the context first` )
+                    ev.emit ( '@shortcuts-error', `Context '${ contextName }' can't be removed during is current active context. Change the context first` )
                     return
             }
         if ( !shortcuts [ contextName ] ) {
-                    ev.emit ( 'shortcuts-error', `Context '${ contextName }' does not exist` )
+                    ev.emit ( '@hortcuts-error', `Context '${ contextName }' does not exist` )
                     return
             }
         delete shortcuts [ contextName ]
