a516fa1699693c6c5194a9fe22a9c6ea52db1a1e
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-21 11:05:42 +0300

    Update 'form' plugin tests

--- test/04-form.test.js
+++ test/04-form.test.js
@@ -67,65 +67,101 @@ let short = shortcuts ();
 
 
 
-describe.only ( 'Form plugin', () => {
+describe ( 'Form plugin', () => {
 
       beforeEach ( async  () => {
-                  short.load ( contextDefinition )
-                  let container = document.createElement ( 'div' )
-                  container.id = 'app'
-                  document.body.appendChild ( container )
-                  await html.publish ( Block, {}, 'app' )
-                  a = false, b = false
-          }) // beforeEach
+                        short.load ( contextDefinition )
+                        let container = document.createElement ( 'div' )
+                        container.id = 'app'
+                        document.body.appendChild ( container )
+                        await html.publish ( Block, {}, 'app' )
+                        a = false, b = false
+                }) // beforeEach
 
 
 
       afterEach ( async  () => {
-                  short.reset ()
-                  a = false, b = false, c = null;
-          }) // afterEach
+                        short.reset ()
+                        a = false, b = false, c = null;
+                }) // afterEach
 
 
       it ( 'Shortcut when plugin is not installed', async () => {
-                  const ls = short.listShortcuts ( 'extend' )
-                  expect ( ls ).to.includes ( 'form : action' )
-          }) // it Shortcuts when plugin is not installed
+                        const ls = short.listShortcuts ( 'extend' )
+                        expect ( ls ).to.includes ( 'form : action' )
+                }) // it Shortcuts when plugin is not installed
 
       
       it ( 'Shortcuts when plugin is enabled', async () => {
-                  short.enablePlugin ( pluginForm )
-                  const ls = short.listShortcuts ( 'extend' )
-                  expect ( ls ).to.includes ( 'FORM:WATCH' )
-                  // Shortcut names are normalized by the plugins!               
-                  expect ( ls ).to.not.includes ( 'form : watch' )                  
-          }) // it Shortcuts when plugin is enabled
+                        short.enablePlugin ( pluginForm )
+                        const ls = short.listShortcuts ( 'extend' )
+                        expect ( ls ).to.includes ( 'FORM:WATCH' )
+                        // Shortcut names are normalized by the plugins!               
+                        expect ( ls ).to.not.includes ( 'form : watch' )                  
+                }) // it Shortcuts when plugin is enabled
 
 
 
       it ( 'Simpler form listener. Only "form:action" defined', async () => {
-                  // Uses predefined 'watch' and 'define' functions
-                  short.enablePlugin ( pluginForm )
-                  let edit = 'none';
+                        // Uses predefined 'watch' and 'define' functions
+                        short.enablePlugin ( pluginForm )
+                        let edit = 'none';
 
-                  const contextExtension = {
-                                  'local' : {
-                                          'form: action' : () => [{
-                                                                    fn : () => edit = 'changed'
-                                                                  , type : 'input'
-                                                                  , timing : 'instant'
+                        const contextExtension = {
+                                        'local' : {
+                                                'form: action' : () => [{
+                                                                        fn : () => edit = 'changed'
+                                                                        , type : 'input'
+                                                                        , timing : 'instant'
                                                                 }]
                                         }
-                            };
-                  short.load ( contextExtension )
-                  short.changeContext ( 'local' )
-                  let input = document.getElementById ( 'name' )
-                  input.focus ()
-                  await userEvent.keyboard ( 'hello' )
-                  await wait ( 50 )
-                  await waitFor ( () => {
-                              expect ( edit ).to.equal ( 'changed' )
-                      }, { timeout: 1000, interval: 12 })
-          }) // it Simpler form listener
+                                };
+                        short.load ( contextExtension )
+                        short.changeContext ( 'local' )
+                        let input = document.getElementById ( 'name' )
+                        input.focus ()
+                        await userEvent.keyboard ( 'hello' )
+                        await wait ( 50 )
+                        await waitFor ( () => {
+                                expect ( edit ).to.equal ( 'changed' )
+                        }, { timeout: 1000, interval: 12 })
+                }) // it Simpler form listener
+
+
+
+        it ( 'Arguments for action function', async () => {
+                         short.enablePlugin ( pluginForm )
+                         let storage = [];
+                         short.setDependencies ({ storage })
+                         const contextExtension = {
+                                         local: {
+                                                         // Access to 'dependencies' on 'form:action' is available to optimize dev experience
+                                                         // if many functions need access to it. Write ones, use everywhere.
+                                                         'form:action' : ({ dependencies }) => [{ 
+                                                                                 fn : ({ target }) => {
+                                                                                        // dependencies are available here as named argument as well
+                                                                                        // But if I have 20 actions, I need to add 20 times 'dependencies'
+                                                                                         const { storage } = dependencies;
+                                                                                         storage.push ({ target })
+                                                                                     }
+                                                                                 , type : 'input'
+                                                                                 , timing : 'instant'
+                                                                         }]
+                                                 }
+                                 } // contextExtension
+                         short.load ( contextExtension )
+                         short.changeContext ( 'local' )
+                         let input = document.getElementById ( 'name' )
+                         input.focus ()
+                         await userEvent.keyboard ( 'hello' )
+                         await wait ( 50 )
+                         await waitFor ( () => {
+                                 expect ( storage.length ).to.be.greaterThan ( 0 )
+                                 const last = storage[storage.length - 1]
+                                 expect ( last.target ).to.be.an.instanceof ( HTMLElement )
+                                 expect ( last.target.value ).toBe ( 'hello' )
+                         })
+             }) // it arguments for action function
 
 
 
